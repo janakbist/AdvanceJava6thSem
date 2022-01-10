fd7853afef2e2b77699c82ff4004d04ebc0e7563
Janak  bista <bistajanak303@gmail.com>
2022-01-10 17:38:41 +0545

    GUIProgramming using java swing

---
+++ Hello.java
@@ -0,0 +1,24 @@
+package np.edu.scst;
+import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+public class Hello extends JFrame{
+    
+   public  Hello() {
+       JPanel jp = new JPanel(); 
+       jp.setBackground(Color.red);
+       add(jp);
+       
+       
+       setVisible(true);
+       setDefaultCloseOperation(EXIT_ON_CLOSE);
+       setSize(500,500);
+       setTitle("Hello");
+    }
+    public static void main(String[] args) {
+        
+        Hello h = new Hello();
+    }
+    
+}
