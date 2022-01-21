c16f1c42c5e40328ced756b387d29b14ace6842f
Janak  bista <bistajanak303@gmail.com>
2022-01-21 18:04:32 +0545

    Network programming fileread from url

---
+++ ReadUrl.java
@@ -0,0 +1,16 @@
+
+package np.edu.scst;
+import java.io.*;
+import java.net.*;
+
+public class ReadUrl {
+    public static void main(String[] args) {
+//        URL file1 = new URL("https://www.google.com/url?sa=i&url=https%3A%2F%2Flogowik.com%2Fbca-bank-central-asia-vector-logo-4636.html&psig=AOvVaw3jOBBYj-2qk7H2oFrufGoz&ust=1642828197647000&source=images&cd=vfe&ved=2ahUKEwj75LmKisL1AhV7HLcAHUlACgUQr4kDegUIARDVAQ");
+//        URLConnection file2 = file1.openConnection();
+//        InputStream input = file2.getInputStream();
+//        while(read()! = -1) {
+//        
+//    }
+    }
+    
+}
